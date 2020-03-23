a970b11bf22a6370065dc9f41dfc16eb682ef860
vladyslav223 <vladyslav223@gmail.com>
2020-03-23 16:33:58 +0200

    Updates

---
+++ precache-manifest.294438c7dc5747db24dedc3433e28905.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ced7de782cd50c2febbba0aaf011d6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf2cf937392dd0c69a9d",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f93edf589fb10f59500c",
+    "url": "/static/css/main.e5df9f05.chunk.css"
+  },
+  {
+    "revision": "bf2cf937392dd0c69a9d",
+    "url": "/static/js/2.7865272e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.7865272e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f93edf589fb10f59500c",
+    "url": "/static/js/main.eaa08208.chunk.js"
+  },
+  {
+    "revision": "448dfc0bf1a0c670143c",
+    "url": "/static/js/runtime-main.c879aa7c.js"
+  }
+]);
