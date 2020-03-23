6fd528eddcfe6042e3b762499d9a8fc471f9980f
vladyslav223 <vladyslav223@gmail.com>
2020-03-23 16:36:09 +0200

    Updates

---
+++ precache-manifest.f4cf175234b0ba666852c056454a2032.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ada3a2dcf7e273b5785d09668c9ded",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bf2cf937392dd0c69a9d",
+    "url": "/portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f93edf589fb10f59500c",
+    "url": "/portfolio/static/css/main.e5df9f05.chunk.css"
+  },
+  {
+    "revision": "bf2cf937392dd0c69a9d",
+    "url": "/portfolio/static/js/2.7865272e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfolio/static/js/2.7865272e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f93edf589fb10f59500c",
+    "url": "/portfolio/static/js/main.eaa08208.chunk.js"
+  },
+  {
+    "revision": "0ea1ecda63ed887af4ab",
+    "url": "/portfolio/static/js/runtime-main.10d1f5cf.js"
+  }
+]);
